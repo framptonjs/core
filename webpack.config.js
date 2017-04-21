6af19a146110c17dff377d3bbd150238122b4e41
Kevin Greene <kgreene@linkedin.com>
2017-04-21 13:28:52 -0700

    Updating build to bundle transpiled files

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const { resolve } = require('path');
+
+module.exports = {
+
+  entry: {
+    'core': './dist/index.js'
+  },
+
+  output: {
+    filename: '[name].js',
+    path: resolve('dist/bundles'),
+    library: '@frampton/core',
+    libraryTarget: 'commonjs'
+  },
+
+  resolve: {
+    extensions: [ '.js' ]
+  }
+};
